21e57e4d49f9dc9631091b5210656e14415bf05d
tiago-mends <tiagomendesdev@gmail.com>
2024-04-18 23:37:43 -0300

    UPDATED in page component - Removed trailing space in the div className in the Home component

--- app/page.tsx
+++ app/page.tsx
@@ -2,7 +2,7 @@ import Table from "@/app/components/table";
 
 export default async function Home() {
   return (
-    <div className="flex flex-col items-center justify-center p-24 ">
+    <div className="flex flex-col items-center justify-center p-24">
       <Table />
     </div>
   );
