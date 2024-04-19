ccae9331a0b71da1fdac8b27929af1d9b656a985
tiago-mends <tiagomendesdev@gmail.com>
2024-04-18 23:36:08 -0300

    UPDATED: Added drop-shadow to navbar in header.tsx
    - Added drop-shadow class to navbar in the header component
    - Updated the styling of the navbar to include shadow effect

--- app/sections/header.tsx
+++ app/sections/header.tsx
@@ -2,7 +2,7 @@ import Link from "next/link";
 
 export default function Header() {
   return (
-    <div className="navbar bg-base-100">
+    <div className="navbar bg-base-100 drop-shadow">
       <Link href="/" className="btn btn-ghost text-xl">
         CountryTrekker
       </Link>
